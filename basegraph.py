48485afef06a77e67d84983886d646dfcb60b2ec
Michael Lamparski <me@this.isnt.really.my.email.org>
2015-04-29 08:56:52 -0400

    Initial commit

---
+++ basegraph.py
@@ -0,0 +1,247 @@
+
+import random
+
+import igraph
+
+from itertools import islice
+
+# TODO: this currently implements some behavior specific to undirected graphs;
+#       abstract this out with a subclass and/or mixin
+
+class BaseGraph:
+
+	# A bit of apps hungarian:
+	#     v: a vertex id (as exposed to user, never changes)
+	#     e: an edge id (as exposed to user, never changes)
+	# rv,re: a 'raw' id (used by igraph, may change)
+
+	# The purpose of these prefixes is to make errors easier to spot.
+	# - methods on self._rg accept and return re and rv --- NEVER e and v.
+	# - values that enter or leave this class are e and v --- NEVER re and rv.
+
+	def __init__(self):
+		self._rg = igraph.Graph()
+
+		self._next_e = 0
+
+		self._rv_map = {}
+		self._v_map  = {}
+		self._re_map = {}
+		self._e_map  = {}
+
+	# TODO: Implement these properly before exposing any 'delete' methods.
+	def _v_from_rv(self, rv): return self._v_map[rv]
+	def _rv_from_v(self, v):  return self._rv_map[v]
+	def _e_from_re(self, re): return self._e_map[re]
+	def _re_from_e(self, e):  return self._re_map[e]
+
+	def add_vertices(self, vs):
+
+		vs = list(vs)
+		if len(vs) != len(set(vs)):
+			raise ValueError('vertex {} specified multiple times'.format(repr(v)))
+
+		for v in vs:
+			if v in self._rv_map:
+				raise ValueError('Vertex {} already in graph!'.format(repr(v)))
+
+		# gather (expected) indices of new vertices
+		rvFirst = self._rg.vcount()
+		rvs = range(rvFirst, rvFirst + len(vs))
+
+		v_rv_pairs = list(zip(vs,rvs))
+		self._rv_map.update(
+			{v:rv for (v,rv) in v_rv_pairs}
+		)
+		self._v_map.update(
+			{rv:v for (v,rv) in v_rv_pairs}
+		)
+
+		self._rg.add_vertices(len(vs))
+
+		assert(rvs.stop == self._rg.vcount())
+
+	def _predict_new_res(self, n):
+		reFirst = self._rg.ecount()
+		return range(reFirst, reFirst + n)
+
+	def _predict_new_rvs(self, n):
+		rvFirst = self._rg.vcount()
+		return range(rvFirst, rvFirst + n)
+
+	def add_edge(self, v1, v2, **kwargs):
+		rv1, rv2 = self._rv_from_v(v1), self._rv_from_v(v2)
+		re, = self._predict_new_res(1)
+
+		e = self._next_e
+		self._next_e += 1
+
+		self._rg.add_edge(rv1, rv2, **kwargs)
+
+		self._re_map[e] = re
+		self._e_map[re] = e
+
+	def vertices(self):
+		return iter(self._rv_map)
+
+	def edges(self):
+		return iter(self._re_map)
+
+	def _incident(self, v, mode):
+		res = self._rg.incident(v, mode=mode)
+		# Make a collection now; We do NOT want lazy evaluation! (the raw indices could get invalidated)
+		return tuple(map(self._e_from_re, res))
+
+	def incident_edges(self, v):
+		return self._incident(v, mode=igraph.ALL)
+	def in_edges(self, v):
+		return self._incident(v, mode=igraph.IN)
+	def out_edges(self, v):
+		return self._incident(v, mode=igraph.OUT)
+
+	def arbitrary_edge(self, v1, v2):
+		# TODO: error cases? (e.g. no such edge?)
+		(rv1, rv2) = (self._rv_from_v(v1), self._rv_from_v(v2))
+		re = self._rg.get_eid(rv1, rv2)
+		return self._e_from_re(re)
+
+	# Get the source and target vertices of an edge.
+	def edge_endpoints(self, e):
+		re = self._re_from_e(e)
+		rvSource = self._rg.es[re].source
+		rvTarget = self._rg.es[re].target
+
+		return self._v_from_rv(rvSource), self._v_from_rv(rvTarget)
+
+	# General method for obtaining the other vertex of an edge...
+	#  but ONLY if the specified vertex is a valid source. (else, error)
+	def edge_target_given_source(self, e, vSource):
+		source,target = self.edge_endpoints(e)
+		if   vSource == source:
+			return target
+		elif vSource == target: # FIXME directed should not allow this
+			return source
+		else:
+			raise ValueError('vertex {} cannot be the source of edge'
+				' {} (which connects {} to {})'.format(vSource,e,source,target))
+
+	# General method for obtaining the other vertex of an edge...
+	#  but ONLY if specified vertex is a valid target. (else, error)
+	def edge_source_given_target(self, e, vTarget):
+		source,target = self.edge_endpoints(e)
+		if   vTarget == target:
+			return source
+		elif vTarget == source: # FIXME directed should not allow this
+			return target
+		else:
+			raise ValueError('vertex {} cannot be the target of edge'
+				' {} (which connects {} to {})'.format(vTarget,e,source,target))
+
+	def cycle_basis(self):
+		''' Generate and return a cycle basis for the graph as a list of paths.
+
+		A cycle basis is a minimal-size set of cycles such that any cycle in the
+		graph can be defined as the XOR sum of cycles in the basis.
+
+		Note that, generally speaking, the cycle basis of a graph is NOT unique.'''
+
+		# Algorithm:
+		# - Make a spanning tree/forest. ANY tree will do.
+		# - For each non-tree edge, traverse the tree between the endpoints to complete a cycle.
+
+		# FIXME: This algo currently assumes the graph is all connected, and just generates
+		#   a single rooted tree (instead of a forest)
+
+		# get tree structure
+		eBackEdges = {}
+		vRoot = self.random_vertex()
+		rvRoot = self._rv_from_v(vRoot)
+		for (vobjNode, distance, vobjPred) in self._rg.bfsiter(rvRoot, mode=igraph.OUT, advanced=True):
+			if vobjPred is None: continue
+			vNode = self._v_from_rv(vobjNode.index)
+			vPred = self._v_from_rv(vobjPred.index) if vobjPred is not None else None
+			eBackEdges[vNode] = self.arbitrary_edge(vPred, vNode)
+
+		# helper method - returns list of edges from root to v
+		def path_from_root(v):
+			result = []
+
+			while v != vRoot:
+				result.append(eBackEdges[v])
+				v = self.edge_source_given_target(eBackEdges[v], v)
+
+			result.reverse()
+			return result
+
+		# build a cycle from each non-tree edge
+		cycles = []
+		eTreeEdges = set(eBackEdges.values())
+		for e in self.edges():
+			if e not in eTreeEdges:
+
+				# find paths from root to each node
+				source, target = self.edge_endpoints(e)
+				sourcePath = path_from_root(source)
+				targetPath = path_from_root(target)
+
+				# length of path to common ancestor
+				iFrom = len(common_prefix(sourcePath, targetPath))
+
+				cycle = [e]                                # source to target (via non-tree edge)
+				cycle.extend(reversed(targetPath[iFrom:])) # target to ancestor (along tree)
+				cycle.extend(sourcePath[iFrom:])           # ancestor to source (along tree)
+
+				cycles.append(cycle)
+
+		return cycles
+
+	def random_vertex(self):
+		print(list(self._rg.vs))
+		rvCount = self._rg.vcount()
+		rv = random.randrange(rvCount)
+		return self._v_from_rv(rv)
+
+	def _igraph_vertex(self, v):
+		rv = self._rv_from_v(v)
+		return self._rg.vs[rv]
+
+	def _igraph_edge(self, e):
+		re = self._re_from_e(e)
+		return self._rg.es[re]
+
+
+def common_prefix(*iterables):
+	if len(iterables) == 0:
+		raise ValueError('no iterables provided!')
+
+	result = []
+	for values in zip(*iterables):
+		if any(v != values[0] for v in values[1:]):
+			break
+		result.append(values[0])
+
+	return result
+
+assert(common_prefix([0,'a',3,2,6,6,7], [0,'a',3,2,9,6]) == [0,'a',3,2])
+assert(common_prefix([0,'a',3,2,6,6,7]) == [0,'a',3,2,6,6,7])
+
+
+g = BaseGraph()
+g.add_vertices([chr(ord('A')+i) for i in range(10)])
+g.add_edge('A', 'B')
+g.add_edge('A', 'H')
+g.add_edge('B', 'C')
+g.add_edge('B', 'E')
+g.add_edge('B', 'I')
+g.add_edge('C', 'F')
+g.add_edge('D', 'F')
+g.add_edge('D', 'G')
+g.add_edge('E', 'F')
+g.add_edge('F', 'G')
+g.add_edge('F', 'I')
+g.add_edge('F', 'I')
+g.add_edge('G', 'J')
+g.add_edge('I', 'J')
+g.add_edge('F', 'H')
+
+
---
+++ circuit.py
@@ -0,0 +1,204 @@
+
+import numpy as np
+
+from basegraph import *
+
+__all__ = [
+	'Component',
+	'Resistor',
+	'Capacitor',
+	'Battery',
+	'Circuit',
+]
+
+# produce a sign factor (+/- 1) based on which side we're traveling an
+#  edge from.  This is to allow circuit components to appropriately
+#  account for direction even though the graph is undirected.
+def _sign_from_source_vertex(endpoints, source):
+	s,t = endpoints
+	if source == s: return  1.0
+	if source == t: return -1.0
+	raise ValueError('source not in endpoints')
+
+# TODO: this ought not to be necessary; a path should be a class object
+#       that is capable of producing this information on its own.
+# (this hackish workaround won't even work in some cases; the edge list
+#  data structure does not allow one to identify the (sole) vertex of a
+#  0 edge path, or to identify the direction of a 1 edge path in an
+#  undirected graph)
+# (it also can't determine the correct order of a 2-cycle in an undirected
+#  graph, but I'll let that slide)
+def _vertices_from_path(g, edgelist):
+	if len(edgelist) < 2: raise RuntimeError('This edge list is too short '
+		'for its vertices to properly be determined.  Tell the developer '
+		'to stop being lazy and implement a proper Path class already!')
+
+	s1,t1 = g.edge_endpoints(edgelist[0])
+	s2,t2 = g.edge_endpoints(edgelist[1])
+
+	# HACK: Identify the SECOND vertex as the one shared by the first two edges;
+	#  the other vertex must be the first one in the path.
+	if   t1 in (s2,t2): first = s1
+	elif s1 in (s2,t2): first = t1
+	else: raise RuntimeError('Path invalid (neither vertex of first edge is in second edge)')
+
+	vs = [first]
+	for edge in edgelist:
+		vs.append(g.edge_target_given_source(edge, vs[-1]))
+
+	assert len(vs) == len(edgelist) + 1
+
+	return vs
+
+# the primary purpose of _vertices_from_path (yield (edge, source) pairs so that we
+#  can determine the direction of edges as we iterate over them)
+def _iter_path_with_sources(g, edgelist):
+	vs = _vertices_from_path(g, edgelist)
+	yield from zip(edgelist, vs[:-1])
+
+# An electrical component which rests on a graph edge.
+class Component:
+	# Create a component given a (source, target) tuple specifying its positive direction.
+	def __init__(self, endpoints):
+		self._endpoints = endpoints
+
+	def direction_sign(self, sourceVertex):
+		return _sign_from_source_vertex(self._endpoints, sourceVertex)
+
+	def resistance(self, sourceVertex):
+		raise NotImplementedError()
+
+	def voltage(self, sourceVertex):
+		raise NotImplementedError()
+
+class Resistor(Component):
+	def __init__(self, resistance, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self._resistance = resistance
+
+	def resistance(self):
+		return self._resistance
+
+	def voltage(self, sourceVertex):
+		return 0.0
+
+class Capacitor(Component):
+	def __init__(self, capacitance, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.capacitance = capacitance
+		self.charge      = 0.0
+
+	def resistance(self):
+		return 0.0
+
+	def voltage(self, sourceVertex):
+		return self.direction_sign(sourceVertex) * self.charge / self.capacitance
+
+# fixed potential source
+class Battery(Component):
+	def __init__(self, potential, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self._potential = potential
+
+	def resistance(self):
+		return 0.0
+
+	def voltage(self, sourceVertex):
+		return self.direction_sign(sourceVertex) * self._potential
+
+
+class Circuit(BaseGraph):
+	def add_resistor(self, v1, v2, resistance):
+		component = Resistor(resistance, (v1,v2))
+		self.add_edge(v1,v2,component=component)
+
+	def add_capacitor(self, v1, v2, capacitance):
+		component = Capacitor(capacitance, (v1,v2))
+		self.add_edge(v1,v2,component=component)
+
+	def add_battery(self, v1, v2, voltage):
+		component = Battery(voltage, (v1,v2))
+		self.add_edge(v1,v2,component=component)
+
+	def edge_component(self, e):
+		return self._igraph_edge(e).attributes()['component']
+
+	def path_total_voltage(self, path):
+		acc = 0.0
+		for e,source in _iter_path_with_sources(self, path):
+			acc += self.edge_component(e).voltage(source)
+		return acc
+
+	def compute_currents(self):
+
+		# Currents are computed using mesh current analysis;
+		# We only compute a current for each cycle in the cycle basis.
+
+		cyclebasis = self.cycle_basis()
+
+		# For each edge, generate a list of (index, sign) for each cycle that crosses it.
+		# This is used to go back and forth between the cycle currents and the individual
+		#  edge currents.
+		cycles_from_edge = {e:[] for e in self.edges()}
+
+		for pathI, path in enumerate(cyclebasis):
+			for e, source in _iter_path_with_sources(self, path):
+				sign = self.edge_component(e).direction_sign(source)
+				cycles_from_edge[e].append((pathI, sign))
+
+		# Generate voltage vector
+		V = np.array([[self.path_total_voltage(path)] for path in cyclebasis])
+
+		# Generate resistance matrix
+		R = np.zeros((len(cyclebasis), len(cyclebasis)))
+
+		for e in self.edges():
+			component = self.edge_component(e)
+			resistance = component.resistance()
+
+			# Multiplying the signs gives +1 between cycles crossing this edge in the
+			#  same direction, and -1 between cycles crossing in reverse directions.
+			for (row, row_sign) in cycles_from_edge[e]:
+				for (col, col_sign) in cycles_from_edge[e]:
+					R[row,col] += row_sign * col_sign * resistance
+
+		# Solve linear system
+		cycle_currents = np.linalg.solve(R,V).reshape([len(cyclebasis)])
+
+		print('cycle currents: {}'.format(cycle_currents))
+
+		# Build result:  a property map of {edge: current}
+		edge_currents = {}
+		for e in self.edges():
+			edge_currents[e] = sum(cycle_currents[cycleId] * sign for cycleId, sign in cycles_from_edge[e])
+
+		return edge_currents
+
+
+def test_two_separate_loops():
+	# A circuit with two connected components.
+	circuit = Circuit()
+	circuit.add_vertices(['a1', 'a2', 'a3', 'b1', 'b2', 'b3'])
+	circuit.add_battery('a1', 'a2', 5.0)
+	circuit.add_resistor('a2', 'a3', 1.0)
+	circuit.add_resistor('a3', 'a1', 1.0)
+	circuit.add_battery('b1', 'b2', 5.0)
+	circuit.add_resistor('b2', 'b3', 1.0)
+	circuit.add_resistor('b3', 'b1', 1.0)
+
+	current = circuit.compute_currents()
+	for v in current.values():
+		assert(abs(v - 2.5) < 1E-10)
+
+def test_two_loop_circuit():
+	circuit = Circuit()
+	circuit.add_vertices(['top', 'bot', 'left', 'right'])
+	circuit.add_battery('bot', 'left',  28.0)
+	circuit.add_battery('bot', 'right',  7.0)
+	circuit.add_resistor('top', 'bot',   2.0)
+	circuit.add_resistor('top', 'left',  4.0)
+	circuit.add_resistor('top', 'right', 1.0)
+	print(circuit.compute_currents())
+
+test_two_separate_loops()
+test_two_loop_circuit()
