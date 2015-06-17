e8d7e8c7fcacb73458c6108dbfd414f8afb5baf1
Michael Lamparski <me@this.isnt.really.my.email.org>
2015-06-17 10:39:26 -0400

    Fill out stubs in cyclebasis_provider

--- cyclebasis_provider.py
+++ cyclebasis_provider.py
@@ -1,4 +1,5 @@
 
+import networkx as nx
 import graph.cyclebasis.planar
 
 __all__ = [
@@ -17,18 +18,35 @@ class planar:
 
 		return graph.cyclebasis.planar.planar_cycle_basis_nx(g, xs, ys)
 
-	def info(self): return {} # XXX
+	def info(self):
+		return {'mode': 'from planar embedding'}
 
 # Loaded from separate file
 class from_file:
-	def __init__(self, path): pass # XXX
 	def is_planar(self): return False
-	def new_cyclebasis(self, g): return None # XXX
-	def info(self): return {} # XXX
+
+	def __init__(self, path):
+		self.path = path
+
+	def new_cyclebasis(self, g):
+		raise RuntimeError('not implemented!') # XXX
+
+	def info(self):
+		return {
+			'mode': 'from file',
+			'path': self.path,
+		}
 
 # Networkx built-in cycle_basis method
 class last_resort:
 	def is_planar(self): return False
-	def new_cyclebasis(self, g): return None # XXX
-	def info(self): return {} # XXX
+
+	def new_cyclebasis(self, g):
+		cycles = list(nx.cycle_basis(g))
+		for c in cycles:
+			c.append(c[0]) # make loop
+		return cycles
+
+	def info(self):
+		return {'mode': 'networkx builtin'}
 
