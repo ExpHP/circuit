7b25de1bd16db9f6563b53b6beb9c1f68258b279
Michael Lamparski <me@this.isnt.really.my.email.org>
2015-06-18 10:23:16 -0400

    Fix build-xorbasis.sh
    Due to a typo it was producing a file called ".so".  YAY BASH

--- build-xorbasis.sh
+++ build-xorbasis.sh
@@ -1,9 +1,10 @@
 set -e
 CFILEBASE=graph/cyclebasis/xorbasis
 CYTHONBASE=graph/cyclebasis/cXorBasis
-CPPFLAGS="-Wall --pedantic -O2 --std=c++11 -fPIC -I/usr/include/python3.4"
+#CPPFLAGS="-Wall --pedantic -O2 --std=c++11 -fPIC -I/usr/include/python3.4"
+CPPFLAGS="-Wall --pedantic -O2 --std=c++11 -fPIC -I/usr/include/python3.4 -DNDEBUG"
 cython ${CYTHONBASE}.pyx --cplus
 clang++ $CPPFLAGS -c ${CFILEBASE}.cpp -o ${CFILEBASE}.o
 clang++ $CPPFLAGS -c ${CYTHONBASE}.cpp -o ${CYTHONBASE}.o
-clang++ $CPPFLAGS --shared -o ${BASENAME}.so ${CFILEBASE}.o ${CYTHONBASE}.o
+clang++ $CPPFLAGS --shared -o ${CYTHONBASE}.so ${CFILEBASE}.o ${CYTHONBASE}.o
 echo 'done'
