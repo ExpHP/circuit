e4ee5da528a00ee62ace0952a3b488e2857b4b1f
Michael Lamparski <me@this.isnt.really.my.email.org>
2015-07-10 15:38:14 -0400

    commit edits to analysis.py
    In hindsight I find analyisis.py's api to be really unflexible and
    annoying to use due to the number of similar functions it provides
    (and *has* to provide in order to be usable)
    
    These edits have been sitting around for a while uncommitted
    because I think they ultimately have an overal negative impact on
    maintainability... but we all know how that story usually ends for
    me

--- analysis.py
+++ analysis.py
@@ -3,7 +3,21 @@
 import copy
 import bisect
 
-from itertools import zip_longest
+from util import zip_variadic, zip_matching_length
+
+import numpy as np
+
+# FIXME this file currently simultaneously tries to support multiple paradigms
+# (a bunch of free functions operating on dicts, versus gratituous use of structured numpy arrays)
+#
+# As a result, some properties of the data can be computed via two completely independent code paths.
+# This is the *bad* kind of redundancy (the kind which can easily become inconsistent!)
+
+def read_info(path):
+	import json
+	with open(path) as f:
+		s = f.read()
+	return json.loads(s)
 
 def trial_defects_stepwise(trial_info):
 	return map(len, trial_info['steps']['deleted'])
@@ -11,12 +25,131 @@ def trial_defects_stepwise(trial_info):
 def trial_defects_cumulative(trial_info):
 	return prefix_sums(trial_defects_stepwise(trial_info), zero=0)
 
+def trial_max_defects_possible(trial_info):
+	# FIXME this assumes there's exactly 2 non-deletable vertices
+	return trial_info['graph']['num_vertices'] - 2
+
+def trial_defects_ratio(trial_info):
+	n = trial_max_defects_possible(trial_info)
+	return [float(x) / n for x in trial_defects_stepwise(trial_info)]
+
 def trial_current(trial_info):
 	return trial_info['steps']['current']
 
 def trial_resistance(trial_info):
 	return map(lambda x: 1./x, trial_current(trial_info))
 
+def trial_array(trial_info):
+	import numpy
+	steps = trial_info['steps']
+	data_iter = zip_matching_length(*steps.values())
+
+	# numpy is REALLY picky about container types
+	data_tuples = list(map(tuple, data_iter))
+
+	# autogenerate record type based on python value types
+	# not sure how reliable this will be versus hardcoded names/types
+	spec = []
+	for name, value in zip(steps.keys(), data_tuples[0]):
+		spec.append((name, type(value)))
+
+	assert all(type(v) == typ for v,(name,typ) in zip(data_tuples[0], spec))
+	return numpy.array(data_tuples, dtype=spec)
+
+def trialset_array(trial_infos):
+	import numpy
+	return numpy.vstack(list(trial_array(info) for info in trial_infos))
+
+# FIXME there's some redundant functionality between this and previously existing "trialset"
+#  functions currently as the two different interfaces kind of work at odds to eachother
+def trialset_augmented_array(trialset_info):
+	arr = trialset_array(trialset_info)
+
+	fields = listify_dtype(arr.dtype)
+	oldnames = [x[0] for x in fields]
+	fields.append(('step', 'i8'))
+	fields.append(('deleted_cum', 'O'))
+	fields.append(('deleted_count', 'i8'))
+	fields.append(('deleted_count_cum', 'i'))
+	fields.append(('deleted_ratio', 'f8'))
+
+	out = np.zeros(arr.shape, dtype=fields)
+	for name in oldnames:
+		out[name] = arr[name]
+
+	for t_out,t_info in zip(out, trialset_info):
+		t_out['step'] = np.arange(len(t_out))
+		t_out['deleted_cum'] = trial_deleted_cum_array(t_info)
+
+	out['deleted_count'] = np.vectorize(len)(out['deleted'])
+	out['deleted_count_cum'] = np.vectorize(len)(out['deleted_cum'])
+
+	out['deleted_ratio'] = np.float64(out['deleted_count_cum']) / trial_max_defects_possible(trialset_info[0])
+	assert (out['deleted_count_cum'] == np.vectorize(len)(out['deleted_cum'])).all()
+	return out
+
+# converts a np.dtype into a python list in the format accepted by np.array
+def listify_dtype(dtype):
+	# np.dtype is not iterable o_O
+	types = [dtype[i] for i in range(len(dtype))]
+	return list(zip(dtype.names, types))
+
+# Provides a complete list of all vertices deleted up to a point for any given step.
+# To avoid O(N^2) memory requirements, each element is actually a slice of a shared master array.
+def trial_deleted_cum_array(trial_info):
+	arr = trial_array(trial_info)
+
+	assert isinstance(arr['deleted'][0], list)
+	pieces = [np.array(x, dtype='O') for x in arr['deleted']]
+	master = np.hstack(pieces)
+
+	lengths = np.array([len(x) for x in pieces])
+	stops   = np.cumsum(lengths)
+	views   = [master[:x] for x in stops]
+
+	return object_array(views, ndim=1)
+
+# Force creation of an object-type numpy array (``dtype='O'``) with the specified number of
+#  dimensions, rather than letting numpy wing it with its crazy value-based magicks:
+#
+# >>> np.array([[1,2],[3,4],[5,6],[7]], dtype='O').shape    # 1d array of lists
+# (4,)
+# >>> np.array([[1,2],[3,4],[5,6],[7,8]], dtype='O').shape  # 2d array of integers  (!!!)
+# (4, 2)
+def object_array(objects, ndim=1):
+	# listify objects in case it is iterable.
+	# keep in mind ``np.array`` cannot be trusted with this as it auto-detects ndim
+	#   and may coerce the actual objects themselves into array if they happen to be
+	#   list-like and of equal length
+	objects = ndim_list_create(objects, ndim)
+	shape = ndim_list_shape(objects, ndim)
+
+	out = np.zeros(shape, dtype='O')
+	out[...] = objects
+	return out
+
+# create an "ndim_list" (a multidimensional list of known depth) from any iterable.
+# lengths are assumed to be equal along each axis.
+def ndim_list_create(src, ndim):
+	assert ndim > 0
+	if ndim == 1:
+		return list(src)
+	else:
+		inner = lambda x: ndim_list_create(x, ndim-1)
+		return list(map(inner, src))
+
+def ndim_list_shape(lst, ndim):
+	assert ndim > 0
+	shape = [None]*ndim
+	for i in range(ndim):
+		shape[i] = len(lst)
+		lst = lst[0]
+	assert None not in shape
+	return tuple(shape)
+
+# Note: because omitting steps from the beginning/middle of the trial may mess
+#  with "cumulative" properties, it's not a very good idea to use this for any
+#  purpose other than cutting stuff off from the end.
 def slice_steps(step_info, *args):
 	sl = slice(*args)
 	return {k:v[sl] for k,v in step_info.items()}
@@ -45,21 +178,13 @@ def trim_trial_by_current(trial_info, threshold=0.0):
 	assert all(abs(x) <= threshold*(1 + 1e-14) for x in arr[zero_idx:])
 
 	result = copy.deepcopy(trial_info)
-	result['steps'] = slice_steps(result['steps'], start=0, stop=zero_idx)
+	result['steps'] = slice_steps(result['steps'], 0, zero_idx)
 	return result
 
 # True if all provided lists contain the same values up to where each is defined
 # (the lists may be of different length)
 def are_lists_consistent(its):
 	return all(map(all_equal, zip_variadic(*its)))
-
-# a version of zip intended for lists of varying length, which returns tuples
-#  of varying length based on how many lists remain.
-def zip_variadic(*its):
-	sentinel = object()
-	def without_fill(xs):
-		return filter(lambda x: x is not sentinel, xs)
-	return (tuple(without_fill(xs)) for xs in zip_longest(*its, fillvalue=sentinel))
 
 # are all elements equal?
 def all_equal(vals):
